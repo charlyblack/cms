acfd71afc29186627611b33dcc4940ce24479cd1
Carl Robinson <charlyblack75@gmail.com>
2013-05-15 17:54:09 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cmsmagic::Application.config.secret_token = 'a9b00ea0f460139d2db4e8bcf85a624990065f684cb3deec3e449b1a86efae5d9fa17ea7795d71127f1462c89153dd4f507dfe652715dbc4b32f07e665ee78c9'
